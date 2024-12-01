933f2e6c22b5aa794d25ed0be71ca73d41e390c9
aoiacai <102672343+aoiacai@users.noreply.github.com>
2024-12-01 14:03:54 +0900

    名所ダミー追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,48 +3,51 @@ import SuggestionBox from "../components/SuggestionBox";
 import { Box } from "@chakra-ui/react";
 
 export default function HomePage() {
-  const suggestions = [
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 1",
-      badge: "新着",
-      description: "このマップは新しい地図です。詳細な情報を確認できます。",
-    },
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 2",
-      badge: "人気",
-      description: "多くのユーザーに人気のある地図です。",
-    },
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 3",
-      badge: "注目",
-      description: "注目のエリアを特集した地図です。",
-    },
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 4",
-      badge: "注目",
-      description: "注目のエリアを特集した地図です。",
-    },
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 3",
-      badge: "注目",
-      description: "注目のエリアを特集した地図です。",
-    },
-    {
-      image: "/map-placeholder.png",
-      title: "おすすめマップ 4",
-      badge: "注目",
-      description: "注目のエリアを特集した地図です。",
-    },
-  ];
+	const suggestions = [
+		{
+			image: "/001_s.jpg",
+			title: "東尋坊",
+			badge: "新着",
+			description: "東尋坊は福井を代表する名所です。ポケモンGoでも有名です。",
+		},
+		{
+			image: "/asakurasi.jpg",
+			title: "朝倉氏遺跡",
+			badge: "人気",
+			description:
+				"朝倉氏遺跡は福井を代表する名所です。ポケモンGoでも有名です。",
+		},
+		{
+			image: "/hotaikonotaki.jpg",
+			title: "五太子の滝",
+			badge: "注目",
+			description:
+				"五太子の滝は福井を代表する名所です。ポケモンGoでも有名です。",
+		},
+		{
+			image: "/youkou.jpg",
+			title: "名勝 養浩館庭園",
+			badge: "注目",
+			description:
+				"養浩館庭園は福井を代表する名所です。ポケモンGoでも有名です。",
+		},
+		{
+			image: "/map-placeholder.png",
+			title: "おすすめマップ 3",
+			badge: "注目",
+			description: "注目のエリアを特集した地図です。",
+		},
+		{
+			image: "/map-placeholder.png",
+			title: "おすすめマップ 4",
+			badge: "注目",
+			description: "注目のエリアを特集した地図です。",
+		},
+	];
 
-  return (
-    <Box>
-      <SuggestionBox suggestions={suggestions} />
-    </Box>
-  );
+	return (
+		<Box>
+			<SuggestionBox suggestions={suggestions} />
+		</Box>
+	);
 }
